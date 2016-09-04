abb961b581308af001a0de1c7042525bc4ebc3bb
Siddharth Patil <sid@sidpatil.com>
2016-09-04 18:07:15 -0400

    Moved LightDM to configuration.nix.

--- suzy.nix
+++ suzy.nix
@@ -3,7 +3,6 @@
 		isNormalUser = true;
 	};
 
-	services.xserver.displayManager.lightdm.enable = true;
 	services.xserver.desktopManager.xfce.enable = true;
 
 	environment.systemPackages = with pkgs; [
